00115c36a86e705666970a00093c192ba45f415b
vikky-data <onwuttuebevictoria@gmail.com>
2026-02-19 23:21:49 +0100

    created engine

---
+++ database.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import session, sessionmaker 
+
+
+engine = create_engine("sqlite.///./student.db")
